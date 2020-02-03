e77b031c09c315544a34137d2d846be25633af81
Bill Bauernschmidt <wbauernschmidt@restorationhardware.com>
2020-02-03 15:57:15 -0700

    Regenerated package-lock

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import * as Keycloak from 'keycloak-js'
+import * as Keycloak from 'keycloak-js';
 
 //keycloak init options
 let initOptions = {
