88279995708f8d8cdb719b547e7566e4df8651e7
Bill Bauernschmidt <wbauernschmidt@restorationhardware.com>
2020-02-06 10:38:03 -0700

    Configuring for sandbox KC

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ import * as Keycloak from 'keycloak-js';
 
 //keycloak init options
 let initOptions = {
-    url:  'https://auth.rhapsody.restorationhardware.com/auth',
+    //url:  'https://auth.rhapsody.restorationhardware.com/auth',
     //url:  'https://auth.rhapsodynonprod.restorationhardware.com/auth',
-    //url:  'https://keycloak.internal.rhapsodysandbox3.net/auth',
+    url:  'https://keycloak.internal.rhapsodysandbox3.net/auth',
     realm: 'production',
-    //clientId: 'react-test-app',
-    clientId: 'rh-mobile-frontend',
+    clientId: 'react-test-app',
+    //clientId: 'rh-mobile-frontend',
     onLoad: 'login-required',
     //pkceMethod: 'S256',
     //promiseType: 'native'
