8d6dfc415ff045e1bdfe0dfb2f820836a2ce3228
Bill Bauernschmidt <wbauernschmidt@restorationhardware.com>
2020-02-11 10:40:59 -0700

    Added call to init ATG session cookies

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import keycloaklogo from './keycloak.png';
 import love from './heart.png';
+import axios from 'axios';
 
 import './App.css';
 //import { withCookies } from 'react-cookie';
@@ -16,7 +17,7 @@ function App() {
         <img src={love} className="Love-logo" alt="heart" />
         <img src={keycloaklogo} className="Keycloak-logo" alt="keycloaklogo" />
         </div>
-       
+
         <a
           className="App-link"
           href="https://medium.com/keycloak"
@@ -44,6 +45,20 @@ function App() {
       </header>
     </div>
   );
+}
+
+initSession () {
+  // curl -v -X POST -H "content-type: application/json" -H "user-agent: (iPhone; CPU rh-mobile-internal-only OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1" -d '{"operationName":"SessionConf","variables":{},"query":"query SessionConf {  sessionConf {    sessionConfirmation    sessionId  }}"}' https://stg4-www.restorationhardware.com/rh-experience-layer-v1/graphql
+  axios.post('https://stg4-www.restorationhardware.com/rh-experience-layer-v1/graphql',
+  {
+    headers: {'content-type': 'application/json', 'user-agent': '(iPhone; CPU rh-mobile-internal-only OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'},
+    data: {
+      operationName: 'SessionConf',
+      variables: {},
+      query: 'query SessionConf {  sessionConf {    sessionConfirmation    sessionId  }}'
+    }
+  })
+  .then(response => console.log(response))
 }
 
 //export default withCookies(App);
